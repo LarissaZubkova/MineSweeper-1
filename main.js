22ca2917a21dcd820649aa3a71feee29a7353b15
Larissa <larissazubkova@gmail.com>
2023-03-03 01:03:17 +0300

    Add bomb

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 import {HEIGHT, WIDTH, BOMB_COUNT} from './consts.js';
 
 const boardElement = document.querySelector('main');
-console.log(boardElement)
-const displayMinesElement = document.querySelector('.mines');
+const displayBombsElement = document.querySelector('.bombs');
 const displayStartBtnElement = document.querySelector('.start-btn');
 const displayTimerElement = document.querySelector('.timer');
+const gameOver = true;
 
-const createGameView = () => {
+function createGameView() {
+    
+
     boardElement.innerHTML = '';
-    displayMinesElement.innerHTML = BOMB_COUNT;
+    displayBombsElement.innerHTML = BOMB_COUNT;
     for (let x = 0; x < WIDTH; x++) {
         let cellElement = document.createElement('div');
         cellElement.setAttribute('class', 'cell');
@@ -22,6 +24,25 @@ const createGameView = () => {
         }
         boardElement.append(cellElement);
     }
+
+    const cells = [...boardElement.querySelectorAll('.field')];
+    console.log(cells)
+    const bombs = [...Array(cells.length).keys()]
+    .sort(() => Math.random() - 0.5).slice(0, BOMB_COUNT);
+    console.log(bombs)
+
+    boardElement.addEventListener('click', (evt) => {
+        if (!evt.target.classList.contains('field')) {
+         return;
+        }
+        
+        const index = cells.indexOf(evt.target);
+        console.log(index)
+        bombs.includes(index) ? evt.target.classList.add('bomb') : '';
+    });
+
 } 
+
+
 createGameView();
 displayStartBtnElement.addEventListener('click', createGameView);
